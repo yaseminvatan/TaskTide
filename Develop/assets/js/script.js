1800113c76904d2b3163361f6631619a47989323
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-04 14:05:26 -0800

    drag-drop function bug fixed

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -48,7 +48,7 @@ function createTaskCard(task) {
 console.log("Loaded tasks from localStorage:", taskList);
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-   $("#todo-cards, #in-progress-cards, #done-cards").empty();
+   $("#to-do-cards, #in-progress-cards, #done-cards").empty();
    taskList.forEach((task) =>{
       const taskCard = createTaskCard(task);
       $(`#${task.status}-cards`).append(taskCard);
@@ -103,16 +103,17 @@ function handleDeleteTask(event){
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
    const taskId = ui.draggable.data("id");
-   const newStatus = $(event.target).attr("id").split("-")[0];
- 
+   const newStatus = $(event.target).closest(".lane").attr("id");
+
    const task = taskList.find((t) => t.id === taskId);
    if (task) {
-     task.status = newStatus;
+      task.status = newStatus;
    }
- 
+
    localStorage.setItem("tasks", JSON.stringify(taskList));
    renderTaskList();
 }
+
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
@@ -124,7 +125,7 @@ $(document).ready(function () {
   $(document).on("click", ".delete-task", handleDeleteTask);
 
   // Make lanes droppable
-  $(".lane .card-body").droppable({
+  $(".lane").droppable({
     accept: ".card",
     drop: handleDrop,
   });
